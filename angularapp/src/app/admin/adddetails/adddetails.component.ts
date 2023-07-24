85d72becefee9b089b22703566428ac0a5c0d530
sakethreddygopu <sakethreddygopu@gmail.com>
2023-07-24 18:54:43 +0000

    adding folders

---
+++ angularapp/src/app/admin/adddetails/adddetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adddetails',
+  templateUrl: './adddetails.component.html',
+  styleUrls: ['./adddetails.component.css']
+})
+export class AdddetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
