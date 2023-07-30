e4ae3744a72c974f6125926184cad8512c936fc6
Miruthubhashini R K <118891492+Miruthubhashini@users.noreply.github.com>
2023-07-30 21:48:47 +0530

    Update adminactivate.service.ts

--- angularapp/src/app/components/services/adminactivate.service.ts
+++ angularapp/src/app/components/services/adminactivate.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { LoginauthService } from '../auth/loginauth.service';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminactivateService {
 
-  constructor() { }
+  constructor(private log:LoginauthService, private route: Router,private auth:AuthService) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean  {
+    if(this.log.isLoggedIn()){
+      if(this.auth.uR=='admin'){
+        return true
+      }
+      alert("Access Denied!")
+      this.log.logout()
+      this.route.navigate(['login'])
+      return false
+    }else{
+      this.route.navigate(['login'])
+      return false;
+    }
+  }
 }
