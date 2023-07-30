1e7034dbaec14ca3b5c571ca40c8bc96dffdc24e
Sahana2019 <116181181+Sahana2019@users.noreply.github.com>
2023-07-30 21:34:00 +0530

    Update addpassengers.service.ts

--- angularapp/src/app/components/user/userservices/addpassengers.service.ts
+++ angularapp/src/app/components/user/userservices/addpassengers.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginauthService } from '../../auth/loginauth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddpassengersService {
 
-  constructor() { }
+  private   baseurl='https://8080-aadaacfafcfdbcdfcefbbcfabeafbabbafdec.project.examly.io/user';
+
+  private baseUrl1="http://localhost:9595/user";
+
+ public status!: boolean;
+ constructor(private http: HttpClient, private log:LoginauthService) { }
+ 
+ addPassenger(data:any){
+  const headers = new HttpHeaders().set('Authorization', this.log.token);
+   console.warn("service called");
+   console.log(data,headers);
+   
+    return this.http.post(`${this.baseurl}/`+`addpassenger`,data,{headers});
+ }
+
+ deletePassenger(id:number):Observable<Object>{
+  const headers = new HttpHeaders().set('Authorization', this.log.token);
+  console.log(id,headers);
+  return this.http.delete(`${this.baseurl}/`+`deletepassenger/`+`${id}`,{headers});
+}
+
 }
