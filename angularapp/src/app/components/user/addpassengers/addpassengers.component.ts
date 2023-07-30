7587bc430ac94ff2550ed54e3dfa1a0f834c1f07
Sahana2019 <116181181+Sahana2019@users.noreply.github.com>
2023-07-30 21:30:29 +0530

    Update addpassengers.component.ts

--- angularapp/src/app/components/user/addpassengers/addpassengers.component.ts
+++ angularapp/src/app/components/user/addpassengers/addpassengers.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { passenger } from '../../datatypes';
+import { AddpassengersService } from '../userservices/addpassengers.service';
+import { BoatslistService } from '../userservices/boatslist.service';
+import { BookingdetailsService } from '../userservices/bookingdetails.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-addpassengers',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddpassengersComponent implements OnInit {
 
-  constructor() { }
-
+  det=new passenger();
+  dataarray:any[]=[];
+  count!: number;
   ngOnInit(): void {
+    console.log("This is Constructor");
+    console.log(this.count);
+    this.det=new passenger();
+    this.bid=this.r.snapshot.params['id'];
+    this.det.bookingId=this.bid;
+    this.dataarray.push(this.det);
+      
+  }
+
+  constructor(private addpassengers:AddpassengersService,private r:ActivatedRoute, private route:Router,public book:BookingdetailsService, private boat:BoatslistService){
+    console.log("This is Constructor");
+    
+    this.count=1
+  }
+  bid!: number;
+  onSubmit(){
+    console.log(typeof(this.dataarray));
+    if(this.count!=this.book.persons){
+      alert("Please enter details of "+this.book.persons+ " people.")
+    }
+    else{
+      
+      this.addpassengers.addPassenger(this.dataarray).subscribe((result:any)=>{
+        console.log(result)
+      });
+      console.log("Above navigate",this.boat.vehid)
+      this.addpassengers.status=true;
+      this.route.navigate(["/user/dashboard/addBooking/",this.boat.vehid]);
+    }
+  }
+
+  removeForm(i: any){
+    this.dataarray.splice(i);
+    this.count-=1
+  }
+  persons=this.book.persons
+  addForm(){
+    if(this.count>=this.persons){
+      alert("You cannot add persons more than "+ this.book.persons ) //
+    }
+    else{
+      this.count+=1
+      this.det=new passenger();
+      this.det.bookingId=this.bid;
+      console.log(this.bid," ",this.det.bookingId);
+      
+      this.dataarray.push(this.det);
+    }
   }
 
 }
