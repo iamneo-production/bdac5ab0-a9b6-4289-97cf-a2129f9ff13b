e4e94b4a2e37a2d9497790979ac7d92eb56a9de8
sakethreddygopu <sakethreddygopu@gmail.com>
2023-07-30 12:34:17 +0000

    adding adddetails and edit

---
+++ angularapp/src/app/components/admin/admineditvehicle/admineditvehicle.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdmineditvehicleComponent } from './admineditvehicle.component';
+
+describe('AdmineditvehicleComponent', () => {
+  let component: AdmineditvehicleComponent;
+  let fixture: ComponentFixture<AdmineditvehicleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdmineditvehicleComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdmineditvehicleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angularapp/src/app/components/admin/admineditvehicle/admineditvehicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admineditvehicle',
+  templateUrl: './admineditvehicle.component.html',
+  styleUrls: ['./admineditvehicle.component.css']
+})
+export class AdmineditvehicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
