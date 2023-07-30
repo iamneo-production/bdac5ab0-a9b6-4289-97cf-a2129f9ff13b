b86da46931af0b7fb05e19520687af8829019af1
Sumantsaieskaspa <126333638+Sumantsaieskaspa@users.noreply.github.com>
2023-07-30 18:52:41 +0530

    Sumant's ProfileRepository

--- springapp/src/main/java/com/examly/springapp/repository/ProfileRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/ProfileRepository.java
@@ -4,6 +4,7 @@ import com.examly.springapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ProfileRepository extends JpaRepository<User,Long> {
 
