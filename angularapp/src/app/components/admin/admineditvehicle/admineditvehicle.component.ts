3146490aead25ae4f5532adc63ca2906e07e63fa
balanimisha-27 <116199307+balanimisha-27@users.noreply.github.com>
2023-07-30 21:26:18 +0530

    Update admineditvehicle.component.ts

--- angularapp/src/app/components/admin/admineditvehicle/admineditvehicle.component.ts
+++ angularapp/src/app/components/admin/admineditvehicle/admineditvehicle.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AddvehicleService } from '../addvehicle.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Addvehicle } from '../../datatypes';
 
 @Component({
   selector: 'app-admineditvehicle',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdmineditvehicleComponent implements OnInit {
 
-  constructor() { }
+  id!:number;
+  vehicles: Addvehicle =new Addvehicle();
 
-  ngOnInit(): void {
+  constructor(private addvehicleService: AddvehicleService,private router: Router,private route:ActivatedRoute){}
+
+  ngOnInit(): void{
+    this.id=this.route.snapshot.params['id'];
+    this.addvehicleService.getVehicleById(this.id).subscribe(data=>{
+    this.vehicles=data;
+    })
+  }
+
+  validation(){
+    if((this.vehicles.vehicleName==null || this.vehicles.vehicleName=='')||(this.vehicles.vehicleAvailableTiming==null || this.vehicles.vehicleAvailableTiming=='') || (this.vehicles.vehicleAddress==null || this.vehicles.vehicleAddress=='') || (this.vehicles.vehicleImageURL==null || this.vehicles.vehicleImageURL=='') || (isNaN(this.vehicles.price)) ||(this.vehicles.vehicleCapacity==null || this.vehicles.vehicleCapacity=='') || (this.vehicles.vehicleDescription==null || this.vehicles.vehicleDescription=='')){
+      alert('Enter all the details....');
+    }
+    else{
+      this.editVehicle();
+    }
+  }
+  
+  editVehicle(){
+    this.addvehicleService.editVehicle(this.id, this.vehicles).subscribe(data =>{
+    console.log(data);
+    alert("Vehicle details submitted successfully")
+    this.goToAddvehicleList();
+    },error => console.log(error));
+  }
+
+  goToAddvehicleList(){
+    this.router.navigate(['/admin/admin-vehicleprofile']);
   }
 
 }
