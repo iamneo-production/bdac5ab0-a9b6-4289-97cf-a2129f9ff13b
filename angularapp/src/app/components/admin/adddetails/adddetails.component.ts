6adb8b40b0aeccdcfacac2655f8fc537f8b8641f
balanimisha-27 <116199307+balanimisha-27@users.noreply.github.com>
2023-07-30 18:08:26 +0530

    Update adddetails.component.ts

--- angularapp/src/app/components/admin/adddetails/adddetails.component.ts
+++ angularapp/src/app/components/admin/adddetails/adddetails.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Addvehicle } from 'src/app/components/datatypes';
+import { AddvehicleService } from '../addvehicle.service';
+
 
 @Component({
   selector: 'app-adddetails',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdddetailsComponent implements OnInit {
 
-  constructor() { }
+  vehicles: Addvehicle =new Addvehicle();
 
-  ngOnInit(): void {
+  constructor(private addvehicleService: AddvehicleService,private router: Router){}
+
+  ngOnInit(): void{
+
+  }
+
+  validation(){
+    if((this.vehicles.vehicleName==null || this.vehicles.vehicleName=='')||(this.vehicles.vehicleAvailableTiming==null || this.vehicles.vehicleAvailableTiming=='') || (this.vehicles.vehicleAddress==null || this.vehicles.vehicleAddress=='') || (this.vehicles.vehicleImageURL==null || this.vehicles.vehicleImageURL=='') || (isNaN(this.vehicles.price)) ||(this.vehicles.vehicleCapacity==null || this.vehicles.vehicleCapacity=='') || (this.vehicles.vehicleDescription==null || this.vehicles.vehicleDescription=='')){
+      alert('Enter all the details....');
+    }
+    else{
+      alert("Vehicle added successfully.")
+      this.addUser();
+      
+    }
+  }
+  
+  addUser(){
+    console.log(this.vehicles)
+    this.addvehicleService.saveVehicle(this.vehicles).subscribe((data:any) =>{
+      console.log(data);
+      this.goToAddvehicleList();
+    },error => console.log(error));
+  }
+
+  onSubmit(){
+    this.validation();
+  }
+
+  goToAddvehicleList(){
+    this.router.navigate(['/admin/admin-vehicleprofile']);
   }
 
 }
