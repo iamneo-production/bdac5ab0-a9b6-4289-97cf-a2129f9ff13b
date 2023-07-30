71792cc8e10e23b76f8fd5ede859014a930bc205
Miruthubhashini R K <118891492+Miruthubhashini@users.noreply.github.com>
2023-07-30 21:56:19 +0530

    Update filter.pipe.ts

--- angularapp/src/app/components/user/userservices/filter.pipe.ts
+++ angularapp/src/app/components/user/userservices/filter.pipe.ts
@@ -5,8 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, filterString: string) {
+    
+    
+    if (value.length === 0 || filterString === '' || filterString === undefined || filterString === null) {
+      return value;
+    }
+
+    const users = [];
+    
+    for (const user of value) {
+      if(user['vehicleName'].toLowerCase().includes (filterString.toLowerCase()) || 
+         user['vehicleAddress'].toLowerCase().includes (filterString.toLowerCase) ||
+         user["price"].toLowerCase().includes (filterString.toLowerCase())) {
+         users.push(user);
+      }
+    }
+    return users;
   }
+
 
 }
