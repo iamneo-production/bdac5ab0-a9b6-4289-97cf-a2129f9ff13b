b28ac541bea67dfb897bac0e1354d8d634155437
Miruthubhashini R K <118891492+Miruthubhashini@users.noreply.github.com>
2023-07-30 21:33:01 +0530

    Update searchbar.component.ts

--- angularapp/src/app/components/user/searchbar/searchbar.component.ts
+++ angularapp/src/app/components/user/searchbar/searchbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-searchbar',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchbarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void{}
+
+enteredSearchValue: string = '';
+
+@Output()
+searchTextChanged: EventEmitter<string>=new EventEmitter<string>();
+
+onSearchTextChanged(){
+  this.searchTextChanged.emit(this.enteredSearchValue)
+}
 
 }
