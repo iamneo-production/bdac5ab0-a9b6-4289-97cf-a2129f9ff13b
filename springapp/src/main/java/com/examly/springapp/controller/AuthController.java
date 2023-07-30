2e73b0468fc2f7eb356b43abb39261ba97b7f17a
keshavgit63 <131645313+keshavgit63@users.noreply.github.com>
2023-07-30 14:54:59 +0530

    Update AuthController.java

---
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -0,0 +1,56 @@
+//package com.example.login.controller;
+package com.examly.springapp.controller;
+
+import com.examly.springapp.dtomodels.LoginModel;
+import com.examly.springapp.dtomodels.UserModel;
+import com.examly.springapp.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/user/signup")
+    public String saveUser(@RequestBody @Valid UserModel userModel){
+        return authService.saveUser(userModel);
+    }
+    @PostMapping("/admin/signup")
+    public String saveAdmin(@RequestBody @Valid UserModel userModel){
+        return authService.saveAdmin(userModel);
+    }
+    @PostMapping("/user/login")
+    public boolean isUserPresent(@RequestBody LoginModel loginModel){
+
+//        System.out.println(loginModel);
+//        System.out.println(loginModel.getPassword().length());
+//        System.out.println(authService.len);
+//        System.out.println(authService.p);
+//        System.out.println(authService.e);
+//        System.out.println(authService.bool);
+        return authService.isUserPresent(loginModel);
+    }
+    @PostMapping("/admin/login")
+    public boolean isAdminPresent(@RequestBody LoginModel loginModel){
+//        System.out.println(loginModel);
+//        System.out.println(loginModel.getPassword().length());
+//        System.out.println(authService.len);
+//        System.out.println(authService.bool);
+        return authService.isAdminPresent(loginModel);
+    }
+
+    @PostMapping("/getid")
+    public long getid(@RequestBody LoginModel loginModel){
+        return authService.getid(loginModel);
+    }
+
+}
