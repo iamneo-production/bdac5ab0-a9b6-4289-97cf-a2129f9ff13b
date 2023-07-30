f821a64186c1ab0ab6c4ae809221ad26656b48ef
Sumantsaieskaspa <126333638+Sumantsaieskaspa@users.noreply.github.com>
2023-07-30 16:19:37 +0530

    Sumant's Component

--- angularapp/src/app/components/user/editprofile/editprofile.component.ts
+++ angularapp/src/app/components/user/editprofile/editprofile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ProfileDetails } from '../../datatypes';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
+import { ProfileService } from '../userservices/profile.service';
 
 @Component({
   selector: 'app-editprofile',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditprofileComponent implements OnInit {
 
-  constructor() { }
+  profileModel : ProfileDetails=new ProfileDetails();
 
-  ngOnInit(): void {
+  constructor(public ps:ProfileService, private auth: AuthService,private route:Router){ }
+
+  ngOnInit(): void{
+    this.ps.getDetailsById(this.auth.userId).subscribe((result:any)=>{
+      console.log(result);
+      this.profileModel=result;
+    })
+  }
+  
+  editProfile= new FormGroup(
+    {
+      fname:new FormControl(),
+      lname:new FormControl(),
+      username:new FormControl(),
+      mobileNumber:new FormControl(),
+      gender:new FormControl(),
+      dOB: new FormControl(String)
+    }
+  ) 
+  
+  date:any
+  onSubmit(){
+    this.date=(this.profileModel.dOB).toString();
+    console.log((this.date).typeOf)
+    console.log(this.profileModel.fname)
+    console.log(this.profileModel);
+    
+    this.ps.updateDetails(this.auth.userId,this.profileModel).subscribe({
+      next:(data: any)=>{
+        console.log(data)
+        alert("Your profile updated successfully!")
+        this.route.navigate(['/user/profile'])
+        },
+        error:(error)=>{
+          alert("Sorry something went wrong!")
+        }
+    })
   }
 
 }
+
