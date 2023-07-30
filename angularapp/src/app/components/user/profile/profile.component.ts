a527be2def1afa28a3e6551a38b0883d908afa9a
Sumantsaieskaspa <126333638+Sumantsaieskaspa@users.noreply.github.com>
2023-07-30 14:21:54 +0530

    Create profile.component.ts

---
+++ angularapp/src/app/components/user/profile/profile.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileDetails } from '../../datatypes';
+import { AuthService } from '../../auth/auth.service';
+import { ProfileService } from '../userservices/profile.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  profileModel : ProfileDetails=new ProfileDetails();
+
+  constructor(private ps:ProfileService,private auth:AuthService){ }
+
+  id=this.auth.userId;
+  ngOnInit(): void{
+    console.log(this.id);
+    
+    this.ps.getDetailsById(this.auth.userId).subscribe((result:any)=>{
+      console.log(result);
+      this.profileModel=result;
+    })
+  }
+
+}
