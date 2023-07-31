37ea2f97741acd061658dea2fe77a000bd02682d
RIFAYASHERIN <84873667+RIFAYASHERIN@users.noreply.github.com>
2023-07-31 20:46:58 +0530

    Update BookingRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/BookingRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/BookingRepository.java
@@ -4,10 +4,16 @@ package com.examly.springapp.repository;
 
 import com.examly.springapp.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+
 import java.util.Optional;
+
+
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking,Long> {
