da1f8c61102266aa6f44fe07d87c5699c24fd6a5
Sumantsaieskaspa <126333638+Sumantsaieskaspa@users.noreply.github.com>
2023-07-31 22:50:12 +0530

    Sumant's Component

--- angularapp/src/app/components/user/editprofile/editprofile.component.spec.ts
+++ angularapp/src/app/components/user/editprofile/editprofile.component.spec.ts
@@ -23,3 +23,32 @@ describe('EditprofileComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
+
+
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { EditprofileComponent } from './editprofile.component';
+
+// describe('EditprofileComponent', () => {
+//   let component: EditprofileComponent;
+//   let fixture: ComponentFixture<EditprofileComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ EditprofileComponent ]
+//     })
+//     .compileComponents();
+//   });
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(EditprofileComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
