605297d50dc63d9db83c224c02ce3cbd8fbc353b
Miruthubhashini R K <118891492+Miruthubhashini@users.noreply.github.com>
2023-07-31 18:43:38 +0530

    Update adminactivate.service.spec.ts

--- angularapp/src/app/components/services/adminactivate.service.spec.ts
+++ angularapp/src/app/components/services/adminactivate.service.spec.ts
@@ -3,14 +3,20 @@ import { TestBed } from '@angular/core/testing';
 import { AdminactivateService } from './adminactivate.service';
 
 describe('AdminactivateService', () => {
+  
   let service: AdminactivateService;
 
   beforeEach(() => {
+    
     TestBed.configureTestingModule({});
     service = TestBed.inject(AdminactivateService);
+    
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+    
   });
+  
 });
+
