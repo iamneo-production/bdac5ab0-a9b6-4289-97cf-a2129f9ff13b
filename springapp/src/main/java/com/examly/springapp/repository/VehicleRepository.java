15695d2702410f1f947d5356bac880c279e44d85
Miruthubhashini R K <118891492+Miruthubhashini@users.noreply.github.com>
2023-07-31 18:55:06 +0530

    Update VehicleRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/VehicleRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/VehicleRepository.java
@@ -1,12 +1,16 @@
+
 package com.examly.springapp.repository;
 
 import com.examly.springapp.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 //import com.example.demo.model.Vehicle;
 
+
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, Long>{
+public interface VehicleRepository extends JpaRepository<Vehicle, Long>
+{
     public Vehicle findByVehicleId(Long vehicleID);
 }
